6b35a9ea0ac735d7a13b41905274e5355c3843cd
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2021-06-27 16:13:01 -0400

    Update QueryStringTest.php

--- tests/QueryStringTest.php
+++ tests/QueryStringTest.php
@@ -7,7 +7,6 @@ class QueryStringTest extends TestCase
     /** @test */
     public function it_parses_the_query_string()
     {
-        $this->withoutExceptionHandling();
 
         $response = $this->get(config('legacy-loader.route_prefix').'/query_string.php?hello_world=Hello World');
 
